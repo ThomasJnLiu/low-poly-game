73807d197a03d87ada26c8f5168d6ac25dbf73bf
Thomas <thomasjnliu@gmail.com>
2020-02-03 12:43:18 -0500

    Add ragdolls, player movement

---
+++ Assets/Scripts/CubeMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour
+{
+    [SerializeField]
+    private Transform playerTransform;
+        public int moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+                playerTransform.Translate(moveSpeed * Input.GetAxis("Horizontal")*Time.deltaTime, 0f, moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);
+
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public Rigidbody playerbody;
+    public Transform playerTransform;
+    public int moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerbody = GetComponentInChildren<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerTransform.Translate(moveSpeed * Input.GetAxis("Horizontal")*Time.deltaTime, 0f, moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);
+        //Debug.Log("speed");
+
+        
+    }
+
+
+}
