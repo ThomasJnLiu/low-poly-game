d71999b577bfbe7f15ee92e5eee25e6fe8f9a1fc
Brian Nguyen <nguyen.brian70@gmail.com>
2020-03-11 09:18:09 -0400

    stuff

---
+++ Assets/FollowCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCharacter : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public GameObject target;
+    public float distance;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, target.transform.position.z - distance);
+
+    }
+}
---
+++ Assets/StayUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StayUp : MonoBehaviour
+{
+    public float targetY;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, targetY, transform.position.z);
+    }
+}
